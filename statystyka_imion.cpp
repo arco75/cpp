27363af11603cc0745994bf8667dbc327e24ff8a
Artur Pelo <pelo.artur@gmail.com>
2024-04-30 09:38:06 +0200

    Skasowano błąd (pozostałość w lini 34-smieci) i zdefiniowania porównanie do 'a' zamiast "a" w w metodzie czyImięDamskie.

--- statystyka_imion.cpp
+++ statystyka_imion.cpp
@@ -1,11 +1,11 @@
 /*
 Program:
-    Napisz program, który na wejściu otrzymuje imiona (polskie) a następnie wypisuje 
+    Napisz program, który na wejściu otrzymuje imiona (polskie) a następnie wypisuje
     jaki procent wśród podanych imion jest imionami damskimi i męskimi.
 
 Wytyczne:
     Imiona stanowią instancję klasy Imie.
-    Metody w klasie muszą umożliwić nadanie wartości początkowej przy tworzeniu obiektu, 
+    Metody w klasie muszą umożliwić nadanie wartości początkowej przy tworzeniu obiektu,
     lub bez wartości początkowej.
     Klasa zawiera setter, który umożliwia nadanie nowego imiona jeśli nie zostało podane.
     Instancje obiektów zapisywanie są na liście obiektów lub wskaźników opartej na wektorach.
@@ -14,62 +14,61 @@ Wytyczne:
 #include <vector>
 #include <string>
 //_____definicja klas
-class Imie{
+class Imie {
 private:
     std::string imie;
 public:
-    Imie(std::string imie){
+    Imie(std::string imie) {
         this->imie = imie;
     };
 
-    Imie(void){
-        imie="";
+    Imie(void) {
+        imie = "";
     };
 
-    void settImie(std::string imie){
-        if(this->imie.length()==0) 
-            this->imie=imie;
+    void settImie(std::string imie) {
+        if (this->imie.length() == 0)
+            this->imie = imie;
     }
-    bool czyImieDamskie(void){
-        if(this->imie[imie.length()-1]=="a") return true; else return false;
+    bool czyImieDamskie(void) {
+        if (this->imie[imie.length() - 1] == 'a') return true; else return false;
     }
 };
 
 //_____MAIN_____
-int main(int argc, char **argv){
+int main(int argc, char** argv) {
 
     short decyzja;
     std::string imie;
     std::vector <Imie*> listaImion;
-    int licznikImionDamskich=0;
-    do{
-        std::cout<<"__________________________\nProgram Statystyka Imion"<<std::endl;
-        std::cout<<" 1 - dodaj imie \n 2 - informacja \n 3 - koniec programu \n ?>";
-        std::cin>>decyzja;
+    int licznikImionDamskich = 0;
+    do {
+        std::cout << "__________________________\nProgram Statystyka Imion" << std::endl;
+        std::cout << " 1 - dodaj imie \n 2 - informacja \n 3 - koniec programu \n ?>";
+        std::cin >> decyzja;
 
         switch (decyzja)
         {
         case 1:
-            std::cout<<"Podaj imie:"; std::cin>>imie;
-            Imie(imie);
-             listaImion.push_back(new Imie(imie));
+            std::cout << "Podaj imie:"; std::cin >> imie;
+            listaImion.push_back(new Imie(imie));
             break;
         case 2:
-            for(int i=0; i<listaImion.size()-1; i++){
-                if(listaImion[i]->czyImieDamskie())
+            for (int i = 0; i < listaImion.size() - 1; i++) {
+                if (listaImion[i]->czyImieDamskie())
                     licznikImionDamskich++;
             }
-            std::cout<<"Imion damskich jest:"<<(float)licznikImionDamskich/listaImion.size()*100;
+            std::cout << "Imion damskich jest:" << (float)licznikImionDamskich / listaImion.size() * 100;
             break;
         default:
-            if(decyzja!=0) std::cout<<"Nie ma takiej opcji\n";
+            if (decyzja != 0) std::cout << "Nie ma takiej opcji\n";
             break;
         };
-    }while(decyzja!=3);
+    } while (decyzja != 3);
 
-    for(int i=0; i<listaImion.size()-1;i++)
+    for (int i = 0; i < listaImion.size() - 1; i++)
         delete(listaImion[i]);
-    
+
     listaImion.clear();
     return 0;
 };
