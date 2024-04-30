007d249be6a72c7b91de44652710e70b632691a4
Artur Pelo <pelo.artur@gmail.com>
2024-04-30 08:18:50 +0200

    Przykłady programów

---
+++ statystyka_imion.cpp
@@ -0,0 +1,12 @@
+/*
+Program:
+Napisz program, który na wejściu otrzymuje imiona (polskie) a następnie wypisuje 
+jaki procent wśród podanych imion jest imionami damskimi i męskimi.
+
+Wytyczne:
+Imiona stanowią instancję klasy Imie.
+Metody w klasie muszą umożliwić nadanie wartości początkowej przy tworzeniu obiektu, 
+lub bez wartości początkowej.
+Klasa zawiera setter, który umożliwia nadanie nowego imiona jeśli nie zostało podane.
+Instancje obiektów zapisywanie są na liście obiektów lub wskaźników opartej na wektorach.
+*/
